c001779eede01ad776a63501603a3279db0e6199
JamieHutton1 <837949@swansea.ac.uk>
2016-12-05 20:52:54 +0000

    Add files via upload

--- ContactPanel.java
+++ ContactPanel.java
@@ -1,76 +1,95 @@
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-import java.util.ArrayList;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import java.awt.GridLayout;
 import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import java.awt.Color;
+import javax.swing.JScrollPane;
 import javax.swing.JLabel;
 
-public class ContactPanel extends JFrame {
 
-	private JPanel contentPane;
+public class ContactPanel extends JFrame {
 
 	/**
 	 * Launch the application.
 	 * @throws Exception 
 	 */
 	public static void main(String[] args) throws Exception {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					ContactPanel frame = new ContactPanel();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-		
-	}
-
-	/**
-	 * Create the frame.
-	 * @throws Exception 
-	 */
-	public ContactPanel() throws Exception {
 		String date = "12/12/12" ;
 		String numOfMessages = "12";
 
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(new GridLayout(10, 0, 0, 0));
-			
 		ArrayList<String> list = ReadContactFile.contactReader();
+
+		JFrame frame = new JFrame();
+
 		
-		for(int i = 0; i < list.size(); i++) {
+		JPanel displayContact = new JPanel();
+		JLabel contactLbl = new JLabel("Contacts");
+
+		for (int i = 0; i < list.size(); i++) {
 			JButton Ai = new JButton(list.get(i));
+			displayContact.add(Ai);
+			JLabel Bi = new JLabel(numOfMessages);
+			displayContact.add(Bi);
+			JLabel Ci = new JLabel(date);
+			displayContact.add(Ci);
 			Ai.setBackground(Color.BLACK);
 			Ai.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
 					Ai.setForeground(Color.WHITE);
 				}
 			});
-			JLabel Bi = new JLabel(date);
-			contentPane.add(Bi);
-			JLabel Ci = new JLabel(numOfMessages);
-			contentPane.add(Ci);
-			
-			contentPane.add(Ai);
-			System.out.println(list.get(i));
-		}	
+		}
+		displayContact.setLayout(new GridLayout(0, 1, 0, 0));
+		JScrollPane scrollPane = new JScrollPane(displayContact);
+		scrollPane.setBounds(0, 70, 250, 330);
 		
 		
-	
-	} 
-	
-	
+		JPanel displayGroups = new JPanel();
+		JLabel groupLbl = new JLabel("Group");
+
+		for (int i = 0; i < list.size(); i++) {
+			JButton Ai = new JButton(list.get(i));
+			displayGroups.add(Ai);
+			JLabel Bi = new JLabel(numOfMessages);
+			displayGroups.add(Bi);
+			JLabel Ci = new JLabel(date);
+			displayGroups.add(Ci);
+			Ai.setBackground(Color.BLACK);
+			Ai.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					Ai.setForeground(Color.WHITE);
+				}
+			});
+		}
+		displayGroups.setLayout(new GridLayout(0, 1, 0, 0));
+		JScrollPane scrollPane1 = new JScrollPane(displayGroups);
+		scrollPane1.setBounds(250, 70, 250, 330);
+
+		JPanel contactTitle = new JPanel();
+		contactTitle.add(contactLbl);
+		contactTitle.setBounds(0, 0, 250, 70);
+
+		JPanel groupTitle = new JPanel();
+		groupTitle.setBounds(250, 0, 250, 70);
+		groupTitle.add(groupLbl);
+
+		JPanel contentPane = new JPanel(null);
+		contentPane.setPreferredSize(new Dimension(500, 400));
+		contentPane.add(scrollPane);
+		contentPane.add(scrollPane1);
+		contentPane.add(contactTitle);
+		contentPane.add(groupTitle);
+
+		frame.setContentPane(contentPane);
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frame.setVisible(true);
+	}
+
+
 }
