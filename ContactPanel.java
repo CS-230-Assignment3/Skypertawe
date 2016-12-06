95464478aa3d534cd76fe505e34ef221620be508
JamieHutton1 <837949@swansea.ac.uk>
2016-12-06 18:31:45 +0000

    Add files via upload

--- ContactPanel.java
+++ ContactPanel.java
@@ -13,31 +13,90 @@ import javax.swing.JLabel;
 
 
 public class ContactPanel extends JFrame {
-	public ContactPanel() {
-	}
+	
+	private ArrayList<String> contactList = new ArrayList<String>();
+	private ArrayList<String> groupList = new ArrayList<String>(); 
 
 	/**
 	 * Launch the application.
 	 * @throws Exception 
 	 */
+	
 	public static void main(String[] args) throws Exception {
-		String date = "12/12/12" ;
-		String numOfMessages = "12";
-
-		ArrayList<String> contactList = ReadContactFile.contactReader();
+		ContactPanel scroll = new ContactPanel();
+		JScrollPane scrollPane = scroll.groupPanel();
+		JScrollPane scrollPane1 = scroll.contactPanel();
 		
-		for (int i = 0; i < contactList.size(); i++) {
-			System.out.print(contactList.get(i));
-			
-		}
 		
 		JFrame frame = new JFrame();
+		JLabel contactLbl = new JLabel("Contacts");
+		JLabel groupLbl = new JLabel("Group");
+		
+		JPanel contactTitle = new JPanel();
+		contactTitle.add(contactLbl);
+		contactTitle.setBounds(0, 0, 250, 70);
+
+		JPanel groupTitle = new JPanel();
+		groupTitle.setBounds(250, 0, 250, 70);
+		groupTitle.add(groupLbl);
+
+		
+
+		
+		JPanel contentPane = new JPanel(null);
+		contentPane.setPreferredSize(new Dimension(500, 400));
+		contentPane.add(scrollPane);
+		contentPane.add(scrollPane1);
+		contentPane.add(contactTitle);
+		contentPane.add(groupTitle);
+
+		frame.setContentPane(contentPane);
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frame.setVisible(true);
+	}
+	
+
+	public JScrollPane groupPanel() throws Exception{
+		JPanel displayGroups = new JPanel();
+		setContactList();
+		for (int i = 0; i < getGroupList().size(); i++) {
+			String[] parts = getGroupList().get(i).split(",");
+			String part1 = parts[0];
+			String part2 = parts[1];
+			String part3 = parts[2];
+			JButton Ai = new JButton(part1);
+			displayGroups.add(Ai);
+			JLabel Bi = new JLabel(part2);
+			displayGroups.add(Bi);
+			if(!part2.replaceAll("\\s+","").equals("0")){
+				Bi.setBackground(Color.CYAN);
+				Bi.setOpaque(true);
+			}
+			JLabel Ci = new JLabel(part3);
+			displayGroups.add(Ci);
+			Ai.setBackground(Color.BLACK);
+			Ai.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					Ai.setForeground(Color.WHITE);
+				}
+			});
+		}
+		displayGroups.setLayout(new GridLayout(0, 1, 0, 0));
+		JScrollPane scrollPane1 = new JScrollPane(displayGroups);
+		scrollPane1.setBounds(250, 70, 250, 330);
+		
+		return scrollPane1;
+		
+	}
+	
+	public JScrollPane contactPanel() throws Exception{
 
 		JPanel displayContact = new JPanel();
-		JLabel contactLbl = new JLabel("Contacts");
-
-		for (int i = 0; i < contactList.size(); i++) {
-			String[] parts = contactList.get(i).split(",");
+		setGroupList();
+		
+		for (int i = 0; i < getContactList().size(); i++) {
+			String[] parts = getContactList().get(i).split(",");
 			String part1 = parts[0];
 			String part2 = parts[1];
 			String part3 = parts[2];
@@ -62,56 +121,24 @@ public class ContactPanel extends JFrame {
 		JScrollPane scrollPane = new JScrollPane(displayContact);
 		scrollPane.setBounds(0, 70, 250, 330);
 		
+		return scrollPane;
 		
-		JPanel displayGroups = new JPanel();
-		JLabel groupLbl = new JLabel("Group");
-
-		for (int i = 0; i < contactList.size(); i++) {
-			String[] parts = contactList.get(i).split(",");
-			String part1 = parts[0];
-			String part2 = parts[1];
-			String part3 = parts[2];
-			JButton Ai = new JButton(part1);
-			displayGroups.add(Ai);
-			JLabel Bi = new JLabel(part2);
-			displayGroups.add(Bi);
-			if(!part2.replaceAll("\\s+","").equals("0")){
-				Bi.setBackground(Color.CYAN);
-				Bi.setOpaque(true);
-			}
-			JLabel Ci = new JLabel(part3);
-			displayGroups.add(Ci);
-			Ai.setBackground(Color.BLACK);
-			Ai.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					Ai.setForeground(Color.WHITE);
-				}
-			});
-		}
-		displayGroups.setLayout(new GridLayout(0, 1, 0, 0));
-		JScrollPane scrollPane1 = new JScrollPane(displayGroups);
-		scrollPane1.setBounds(250, 70, 250, 330);
-
-		JPanel contactTitle = new JPanel();
-		contactTitle.add(contactLbl);
-		contactTitle.setBounds(0, 0, 250, 70);
-
-		JPanel groupTitle = new JPanel();
-		groupTitle.setBounds(250, 0, 250, 70);
-		groupTitle.add(groupLbl);
-
-		JPanel contentPane = new JPanel(null);
-		contentPane.setPreferredSize(new Dimension(500, 400));
-		contentPane.add(scrollPane);
-		contentPane.add(scrollPane1);
-		contentPane.add(contactTitle);
-		contentPane.add(groupTitle);
-
-		frame.setContentPane(contentPane);
-		frame.pack();
-		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		frame.setVisible(true);
 	}
-
+	
+	public ArrayList<String> setContactList() throws Exception {
+		return contactList = ReadContactFile.contactReader();
+	}
+	
+	public ArrayList<String> setGroupList() throws Exception {
+		return groupList = ReadContactFile.contactReader(); //reads in groups list here instead of contact list
+	}
+	public ArrayList<String> getGroupList() {
+		return contactList;
+	}
+	
+	public ArrayList<String> getContactList() {
+		return contactList;
+	}
+	
 
 }
