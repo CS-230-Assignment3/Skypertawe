843cb46787cd846aa836d0c5e2e4da07c53303ad
Samuel John O'Reilly <samjo2521@gmail.com>
2016-12-07 16:20:12 +0000

    Started SearchResultsPanel

--- SearchResultsPanel.java
+++ SearchResultsPanel.java
@@ -1,8 +1,23 @@
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by Sam on 07/12/2016.
  */
 public class SearchResultsPanel extends JFrame {
+    private AccountsGraph m_accountsGraph;
+    private JPanel searchPanel;
+    private JPanel resultsPanel;
 
+    public SearchResultsPanel(AccountsGraph accountsGraph) {
+        m_accountsGraph = accountsGraph;
+
+        this.setTitle("Search");
+        this.setSize(500, 400);
+        this.setDefaultCloseOperation(JInternalFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setLayout(null);
+
+
+    }
 }
