84897eb43c8aeeb86753d91ce4f5be7471940235
Hazukiy <Hazukiy@hotmail.co.uk>
2016-12-07 15:42:01 +0000

    Working on GUI for login, mainwindow and welcome panel

--- WelcomePanel.java
+++ WelcomePanel.java
@@ -8,36 +8,57 @@ public class WelcomePanel extends JFrame implements ActionListener {
 	private static final long serialVersionUID = 1L;
 
 	public WelcomePanel() {
-		this.setTitle("Skypeartawe");
+		this.setTitle("Skypertawe");
 		this.setSize(1000, 700);
 		this.setDefaultCloseOperation(JInternalFrame.EXIT_ON_CLOSE);
 		this.setLayout(null);
 		this.setResizable(false);
 		this.setVisible(true);
-		loadPanel();
+		loadAssets();
 	}
 
-	private void loadPanel() {
+	private void loadAssets() {
 		JPanel panel = new JPanel();
-		panel.setBackground(new Color(0, 0, 0, 190));
 		panel.setSize(this.getWidth(), this.getHeight());
 		panel.setLayout(null);
 
-		JLabel title = new JLabel("Skypeartwe");
+		JLabel title = new JLabel("Skypertawe");
 		JButton loginBtn = new JButton("Login");
 		JButton regBtn = new JButton("Register");
 
 		title.setFont(new Font("Arial_Thin", Font.PLAIN, 50));
-		title.setForeground(new Color(255,255,255,255));
 		title.setBounds((this.getWidth() / 3 + 20), 150, this.getWidth(), 55);
 
 		loginBtn.setBounds((this.getWidth() / 3), this.getHeight() / 3, 150, 80);
 		regBtn.setBounds((this.getWidth() / 3 + 160), this.getHeight() / 3, 150, 80);
 
+		loginBtn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				loadLogin();
+			}
+		});
+
+		regBtn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				loadRegister();
+			}
+		});
+
 		panel.add(title);
 		panel.add(loginBtn);
 		panel.add(regBtn);
 		this.add(panel);
+	}
+
+	private void loadLogin() {
+		new LoginPanel();
+		this.dispose();
+	}
+
+	private void loadRegister() {
+
 	}
 
 	@Override
