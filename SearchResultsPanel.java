83b780cab51092e04d7e734c410d41f8b0e6aea1
JamieHutton1 <837949@swansea.ac.uk>
2016-12-08 00:50:35 +0000

    Add files via upload

--- SearchResultsPanel.java
+++ SearchResultsPanel.java
@@ -1,51 +1,67 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SearchResultsPanel extends JFrame {
-    private AccountsGraph m_accountsGraph;
 
-    //Just to test
     public SearchResultsPanel() {
         this.setTitle("Search");
-        this.setSize(250, 500);
-        this.setDefaultCloseOperation(JInternalFrame.DISPOSE_ON_CLOSE);
-        this.setResizable(false);
-        this.setLayout(null);
-        this.setLocationRelativeTo(null);
-        this.setVisible(true);
-        loadAssets();
+		this.setContentPane(buildPanel());
+		this.setSize(250, 400);
+		this.setDefaultCloseOperation(JInternalFrame.EXIT_ON_CLOSE);
+		this.setLayout(null);
+		this.setResizable(false);
+		this.setLocationRelativeTo(null);
+		this.setVisible(true);
     }
+    
+    public JPanel buildPanel () {
+		
+    	AccountsGraph searchResults = new AccountsGraph();
+    	JPanel contentPane = new JPanel(null);
+    	 JLabel searchLabel = new JLabel("Search");
+         searchLabel.setBounds(30, 30, 50, 20);
 
-
-    public SearchResultsPanel(AccountsGraph accountsGraph) {
-        this.setTitle("Search");
-        this.setSize(250, 500);
-        this.setDefaultCloseOperation(JInternalFrame.DISPOSE_ON_CLOSE);
-        this.setResizable(false);
-        this.setLayout(null);
-        this.setLocationRelativeTo(null);
-        this.setVisible(true);
-        m_accountsGraph = accountsGraph;
-        loadAssets();
+         JTextField searchBox = new JTextField(20);
+         searchBox.setBounds(100, 30, 130, 20);
+        
+         
+         JButton submitButton = new JButton("Submit");
+         submitButton.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					String content = searchBox.getText();
+					Account newAccount = searchResults.findAccount(content);
+					String newLine = newAccount.getUser() + "," + newAccount.getFirst() + "," + newAccount.getLastName();
+					JButton ResultLinkButton = new JButton(newLine);
+					ResultLinkButton.setBounds(0, 150, 250, 50);
+					contentPane.add(ResultLinkButton);
+					ResultLinkButton.addActionListener(new ActionListener() {
+							public void actionPerformed(ActionEvent e) {
+								System.out.print("it works, kinda. Needs to vaildate input at graph!");						
+							}
+						});
+				}
+			});
+         
+        submitButton.setBounds(100, 70, 130, 20);
+         
+        JLabel displayLabel = new JLabel("Results");
+        displayLabel.setBounds(30, 100, 50, 20);
+        
+    	contentPane.setPreferredSize(new Dimension(250, 400));
+		contentPane.add(searchLabel);	
+		contentPane.add(searchBox);	
+		contentPane.add(displayLabel);
+		contentPane.add(submitButton);
+	
+		
+		return contentPane;	
     }
-
-    private void loadAssets() {
-        JPanel searchPanel = new JPanel();
-
-        searchPanel.setBounds(0, 0, 250, 500);
-
-        JLabel searchLabel = new JLabel("Search");
-        searchLabel.setBounds(110, 10, 10, 10);
-
-        JTextField searchBox = new JTextField();
-        searchBox.setBounds(125, 10, 20, 10);
-
-        JButton searchButton = new JButton();
-        searchPanel.add(searchLabel);
-        searchPanel.add(searchBox);
-        searchLabel.add(searchButton);
-
-        this.add(searchPanel);
-        this.setContentPane(searchPanel);
-    }
+    
+    
+	public static void main(String[] args){
+		new SearchResultsPanel();
+		
+	}
 }
