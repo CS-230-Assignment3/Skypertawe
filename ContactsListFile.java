7848266f3fff46933b752ba74552c8140998bdaf
Samuel John O'Reilly <samjo2521@gmail.com>
2016-12-09 19:29:55 +0000

    Added doxygen comments

--- ContactsListFile.java
+++ ContactsListFile.java
@@ -3,41 +3,59 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
- * Created by Sam on 06/12/2016.
+ * @file ContactsListFile.java
+ * @author Samuel O'Reilly (824712)
+ * @date 01 Dec 2016
+ * @see Account.java for Account objects
  */
 public class ContactsListFile {
     private final String CONTACTS_LIST_FILEPATH = "graphFiles\\contact.txt";
     private final String TEMP_CONTACTS_LIST_FILEPATH = "graphFiles\\contactsTemp.txt";
 
+    /**
+     * Default constructor
+     */
     public ContactsListFile() {
-
     }
 
+    /**
+     * Adds Account objects to the friends list of other Account objects if the relationship is present in contacts file
+     *
+     * @param accounts list of all Account objects to add friends too
+     */
     public void formContactsList(ArrayList<Account> accounts) {
         File contactsList = new File(CONTACTS_LIST_FILEPATH);
+        //Scanner used to read each line of contacts file
         Scanner in;
         try {
             in = new Scanner(contactsList);
+            //Loop while there is an unread line in contacts file
             while (in.hasNext()) {
+                //Scanner used to parse through the current line in contacts file, and get two usernames(these two are friends)
                 Scanner contactsScanner = new Scanner(in.nextLine());
                 contactsScanner.useDelimiter(",");
                 String firstUsername = contactsScanner.next();
                 String secondUsername = contactsScanner.next();
                 Account firstAccount = null;
                 Account secondAccount = null;
+
                 boolean found = false;
                 int curIndex = 0;
-
+                //Loop through accounts until both accounts are found, or end of list is reached
                 while (!found || curIndex < accounts.size()) {
+                    //If first username in contacts file equals current account username
                     if (firstUsername.equals(accounts.get(curIndex).getUser())) {
                         firstAccount = accounts.get(curIndex);
+                        //If second username in contacts file equals current account username
                     } else if (secondUsername.equals(accounts.get(curIndex).getUser())) {
                         secondAccount = accounts.get(curIndex);
                     }
                     curIndex++;
+                    //Set found to true when both first and second accounts are found
                     found = firstAccount != null && secondAccount != null;
                 } // end while
 
+                //Add each account to the friends list of the other
                 firstAccount.addFriend(secondAccount);
                 secondAccount.addFriend(firstAccount);
                 contactsScanner.close();
@@ -46,21 +64,30 @@ public class ContactsListFile {
             in.close();
 
         } catch (FileNotFoundException fileNotFoundEx) {
-            // TODO: 06/12/2016 catch
+            System.err.println("Cannot find file " + CONTACTS_LIST_FILEPATH +
+                    " " + fileNotFoundEx.getStackTrace());
         }
     }
 
+    /**
+     * Adds the contact relationship between two accounts to the contacts file. Does not add
+     * contacts to each others friends list. This is handled in AccountsGraph.
+     *
+     * @param firstAccount  account to add to file
+     * @param secondAccount account to add to file
+     */
     public void addContactToFile(Account firstAccount, Account secondAccount) {
         File contactsList = new File(CONTACTS_LIST_FILEPATH);
         FileWriter writer = null;
         try {
+            //Writer appends to contacts file
             writer = new FileWriter(contactsList, true);
 
             String newLine;
             // If firstAccount username <= secondAccount username
             if (firstAccount.getUser().compareTo(secondAccount.getUser()) <= 0) {
                 newLine = firstAccount.getUser() + "," + secondAccount.getUser();
-            } else {
+            } else { // Else firstAccount username > secondAccount username
                 newLine = secondAccount.getUser() + "," + secondAccount.getUser();
             }
             writer.write(newLine + "\n");
@@ -70,6 +97,11 @@ public class ContactsListFile {
         }
     }
 
+    /**
+     * Removes contact relationship between two accounts from the contacts file
+     * @param firstAccount account to remove from second account
+     * @param secondAccount account to remove from first account
+     */
     public void removeContactFromFile(Account firstAccount, Account secondAccount) {
         File originalFile = new File(CONTACTS_LIST_FILEPATH);
         File tempFile = new File(TEMP_CONTACTS_LIST_FILEPATH);
@@ -79,8 +111,9 @@ public class ContactsListFile {
             System.err.println(e.getMessage());
             return;
         }
-
+        //Scanner used to read each line of accounts file
         Scanner reader = null;
+        //Writer used to write each line of accounts file
         PrintWriter writer = null;
         try {
             reader = new Scanner(originalFile);
@@ -90,35 +123,35 @@ public class ContactsListFile {
                     + " " + notFound.getMessage());
             return;
         }
-
+        //Loop while there is an unread line in accounts file
         while (reader.hasNext()) {
+            //Scanner used to parse each account
             Scanner contacts = new Scanner(reader.nextLine());
             contacts.useDelimiter(",");
             String firstUsername = contacts.next();
             String secondUsername = contacts.next();
+            //Determines if line should not be written to file
             boolean skipLine = false;
 
+            /*If the two usernames we are removing friends from equal the current accounts
+            * on the line in contacts file, skip writing this line
+             */
             if (firstUsername.equals(firstAccount.getUser()) &&
                     secondUsername.equals(secondAccount.getUser())) {
                 skipLine = true;
-                firstAccount.removeFriend(secondAccount);
-                secondAccount.removeFriend(firstAccount);
-
             } else if (secondUsername.equals(firstAccount.getUser()) &&
                     firstUsername.equals(secondAccount.getUser())) {
                 skipLine = true;
             }
-
             contacts.close();
-
+            //If line should not be skipped, write it
             if (!skipLine) {
                 writer.println(firstUsername + "," + secondUsername);
             }
         }
-
         reader.close();
         writer.close();
-
+        //Delete original accounts file and rename temporary to original file name
         originalFile.delete();
         tempFile.renameTo(originalFile);
 
