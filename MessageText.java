9574f27950d230bafb8c64a4a8af5e4cc191f2c7
Samuel John O'Reilly <samjo2521@gmail.com>
2016-12-11 16:09:24 +0000

    Finished message text

--- MessageText.java
+++ MessageText.java
@@ -3,17 +3,21 @@ import java.util.ArrayList;
 public class MessageText extends Message {
     private String m_text;
 
-    public MessageText(Account accountSend, Account accountRecieve, String msgText) {
-        super(accountSend, accountRecieve);
+    public MessageText(Account accountSend, String msgText) {
+        super(accountSend);
         m_text = msgText;
     }
 
-    public MessageText(Account accountSend, ArrayList<Account> accountRecieve, String msgText) {
-        super(accountSend, accountRecieve);
+    public MessageText(Account accountSend, String msgText, String timestamp) {
+        super(accountSend, timestamp);
         m_text = msgText;
+    }
+
+    public String getText() {
+        return m_text;
     }
 
     public String display() {
-        return m_accountSend + ": " + m_text;
+        return m_accountSend.getUser() + ": " + m_text;
     }
 }
