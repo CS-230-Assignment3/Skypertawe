26c7ffc0b57fb826c9c03554bd1abcc70302669e
Samuel John O'Reilly <samjo2521@gmail.com>
2016-12-11 16:09:37 +0000

    Finished message url

--- MessageURL.java
+++ MessageURL.java
@@ -4,17 +4,17 @@ import java.util.ArrayList;
  * Created by Sam on 11/12/2016.
  */
 public class MessageURL extends DescriptionMessage {
-    public MessageURL(Account accountSend, Account accountRecieve, String path, String description) {
-        super(accountSend, accountRecieve, path, description);
+    public MessageURL(Account accountSend, String path, String description) {
+        super(accountSend, path, description);
     }
 
-    public MessageURL(Account accountSend, ArrayList<Account> accountRecieve, String path, String description) {
-        super(accountSend, accountRecieve, path, description);
+    public MessageURL(Account accountSend, String path, String description, String timestamp) {
+        super(accountSend, path, description, timestamp);
     }
 
     public String display() {
         String line = "";
-        line = m_accountSend + ": " + m_path + " Description:\n " + m_textDescription;
+        line = m_accountSend.getUser() + ": " + m_path + " Description:\n " + m_textDescription;
         return line;
     }
 }
