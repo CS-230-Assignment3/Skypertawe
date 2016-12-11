db585c376b7f3742f2f8a55ae971b0bc03e85a83
Samuel John O'Reilly <samjo2521@gmail.com>
2016-12-11 16:07:24 +0000

    Uncommented accounts

--- Main.java
+++ Main.java
@@ -3,9 +3,9 @@ public class Main {
         AccountsGraph graph = new AccountsGraph();
         //new WelcomePanel(graph);
 
-        //Account myAccount = new Account("Luke", "Luke", "Harvey", "", "16/11/94", "Bristol", "password", 74279);
-        //Account otherAccount = new Account("John", "Cena", "test", "", "16/11/94", "Bristol", "password", 74279);
+        Account myAccount = new Account("Luke", "Luke", "Harvey", "", "16/11/94", "Bristol", "password", 74279);
+        Account otherAccount = new Account("John", "Cena", "test", "", "16/11/94", "Bristol", "password", 74279);
 
-        new ChatPanel(myAccount, otherAccount);
+        new ChatPanel(myAccount, otherAccount, graph);
     }
 }
