c90e8e6981ef47ec02f4ba6b3704d8b618d46793
Samuel John O'Reilly <samjo2521@gmail.com>
2016-12-11 16:07:05 +0000

    Finished description message, removed recieve accounts (not needed)

--- DescriptionMessage.java
+++ DescriptionMessage.java
@@ -12,39 +12,40 @@ public abstract class DescriptionMessage extends Message {
 
     /**
      * Sets the account message is sent from, account message is to be sent to, the filepath to the item being sent
-     * and a description of what is being sent
+     * and a description of what is being sent. This sets the send time to current system time
      *
      * @param accountSend     account sending the message
-     * @param accountRecieve  account receiving the message
      * @param path        path to item being sent
      * @param textDescription short description of file being sent
      * @see Message.java
      */
-    DescriptionMessage(Account accountSend, Account accountRecieve, String path, String textDescription) {
+    DescriptionMessage(Account accountSend, String path, String textDescription) {
         //Pass parameter to superclass
-        super(accountSend, accountRecieve);
+        super(accountSend);
         //Assign member variables
         m_path = path;
         m_textDescription = textDescription;
     }
 
     /**
-     * Sets the account message is sent from, and the ArrayList of Accounts the message is being sent to, the path to the
-     * item being sent and a description of what is being sent
+     * Sets the account message is sent from, account message is to be sent to, the filepath to the item being sent
+     * and a description of what is being sent.
+     * This is given a timestamp to set the message send time to.
      *
      * @param accountSend     account sending the message
-     * @param accountRecieve  ArrayList of accounts receiving the message
-     * @param path            path to item being sent
+     * @param path        path to item being sent
      * @param textDescription short description of file being sent
+     * @param timestamp time to set message send time to
      * @see Message.java
      */
-    DescriptionMessage(Account accountSend, ArrayList<Account> accountRecieve, String path, String textDescription) {
+    DescriptionMessage(Account accountSend, String path, String textDescription, String timestamp) {
         //Pass parameter to superclass
-        super(accountSend, accountRecieve);
+        super(accountSend, timestamp);
         //Assign member variables
         m_path = path;
         m_textDescription = textDescription;
     }
+
 
     /**
      * Sets the filepath of file being sent
